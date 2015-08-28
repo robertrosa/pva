c361e1f787012fcc7a7eeb6b194f04d4d2a9f2c0
robertrosa <robert.rosa@kantarworldpanel.com>
2015-08-28 15:40:12 +0100

    Commit 28/08/2015

---
+++ db/queries/ActiveServersLast24h.sql
@@ -0,0 +1,42 @@
+/*Active servers last 24 hours by BuildTimeStart*/
+select Count(DISTINCT ServerName), dateadd(HH, datediff(HH,0, BuildTimeStart), 0) As Hour
+from pva_production
+where period = 201508 and BuildTimeStart > dateadd(HH, -24, getdate())
+group by dateadd(HH, datediff(HH,0, BuildTimeStart), 0)
+order by dateadd(HH, datediff(HH,0, BuildTimeStart), 0)
+
+
+/*Active servers last 24 hours by BuildTimeStart and BuildTimeStart+BuildSeconds*/
+select count(DISTINCT t.ServerName), t.Hour
+from (select ServerName, dateadd(HH, datediff(HH,0, BuildTimeStart), 0) As Hour
+from pva_production
+where period = 201508 and BuildTimeStart > dateadd(HH, -24, getdate())
+union all
+select ServerName, dateadd(HH, datediff(HH, 0, dateadd(ss, BuildSeconds, BuildTimeStart)), 0) as Hour
+from pva_production
+where period = 201508 and BuildTimeStart > dateadd(HH, -24, getdate())) t
+Group By t.Hour
+
+
+/*Test*/
+select Count(DISTINCT ServerName), dateadd(HH, datediff(HH, 0, dateadd(ss, BuildSeconds, BuildTimeStart)), 0) as Hour
+from pva_production
+where period = 201508 and BuildTimeStart > dateadd(HH, -24, getdate())
+group by dateadd(HH, datediff(HH, 0, dateadd(ss, BuildSeconds, BuildTimeStart)), 0)
+order by dateadd(HH, datediff(HH, 0, dateadd(ss, BuildSeconds, BuildTimeStart)), 0)
+
+
+/*Test*/
+select Count(DISTINCT ServerName), dateadd(HH, datediff(HH, 0, dateadd(ss, BuildSeconds, BuildTimeStart)), 0) as Hour
+from pva_production
+where period = 201508 and BuildTimeStart > dateadd(HH, -26, getdate())
+group by dateadd(HH, datediff(HH, 0, dateadd(ss, BuildSeconds, BuildTimeStart)), 0)
+order by dateadd(HH, datediff(HH, 0, dateadd(ss, BuildSeconds, BuildTimeStart)), 0)
+
+
+
+
+
+
+
+
